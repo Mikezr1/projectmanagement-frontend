7cc34118ffa517be27c83420667da13f1c3d7518
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-11-12 14:12:29 +0100

    Merge conflict fixed app.tsx and router.tsx

--- projectmanagement/src/App.tsx
+++ projectmanagement/src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css'
 import LoginPage from "./pages/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 
 function App() {
 
+  const queryClient = new QueryClient();
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} /> 
-      </Routes>
-    </Router>
+          <QueryClientProvider client={queryClient}>
+            <Router>
+              <Routes>
+                <Route path="/" element={<LoginPage />} /> 
+              </Routes>
+            </Router>
+          </QueryClientProvider>
   )
 }
 
