8f1af5f6f5b9d1f26b3e7990e3e1433163584008
Chun <beerdsenc@gmail.com>
2025-11-18 13:06:35 +0100

    Added fetchComments

---
+++ projectmanagement/src/components/FetchComments.tsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query"
+import commentService from "../services/commentService"
+import type { CommentSummaryDTO } from "../types/models"
+
+
+export default function FetchComments() {
+    const {data: comments, isLoading, isError, error} = useQuery<CommentSummaryDTO[], Error>({
+            queryKey: ['comments'],
+            queryFn: commentService.getAllComments
+        })
+    
+        if (isLoading) return <div>Loading</div>
+        if (isError) return <div>Error: { error?.message }</div>
+
+        return (
+            <>
+            
+            </>
+        )
+}
