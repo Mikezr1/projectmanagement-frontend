fd657d74f56cfc2e3957c91ece60a1315b5dbdce
ThomazSDA <thomas_wennink@hotmail.com>
2025-11-20 12:15:50 +0100

    Final bug fix?

--- projectmanagement/src/pages/LandingPage.tsx
+++ projectmanagement/src/pages/LandingPage.tsx
@@ -1,27 +1,31 @@
-// import { useNavigate } from "react-router-dom";
 import LoginModal from "../modals/LoginModal";
 
 const LandingPage = () => {
+  return (
+    <div className="py-10 bg-black px-6 min-h-screen">
+      <div className="grid grid-cols-2 gap-8">
+        {/* Linker kolom */}
+        <div className="flex flex-col justify-center gap-4">
+          <h3 className="text-bold text-2xl text-white">
+            Start managing your projects
+          </h3>
+          <p className="text-sm text-white">
+            This application will help you manage your projects better than ever before.
+          </p>
 
-    // const navigate = useNavigate();
-
-    return (
-        <div className="py-10 bg-black px-6">
-            <div className="grid grid-cols-2 gap-8">
-                <div className="flex flex-col align-left py-2 gap-4">
-                    <h3 className="text-bold text-2xl text-white">Start managing your projects</h3>
-                    <p className="text-sm text-white" >This application will help you manage your projects better then ever before.</p>
-                    {/* <button className="" type="button" onClick={() => navigate("/login")}> */}
-                    <LoginModal />
-                </div>
-                <div>
-                    <div className="bg-[#636363] aspect-video">
-                        <img src="null" alt="Image of finished product" />
-                    </div>
-                </div>
-            </div>
+          {/* Login knop via LoginModal */}
+          <LoginModal />
         </div>
-    )
-}
+
+        {/* Rechter kolom */}
+        <div className="flex justify-center items-center">
+          <div className="bg-[#636363] aspect-video w-full max-w-md flex justify-center items-center">
+            <img src="null" alt="Image of finished product" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
