714b47de335214dfb9b5f2c646549970a43e9e72
Mikezr1 <74431214+Mikezr1@users.noreply.github.com>
2025-11-21 13:47:58 +0100

    breadcrumb styling

--- projectmanagement/src/components/goToProject.tsx
+++ projectmanagement/src/components/goToProject.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function GoToProject({ projectId, task}) { 
     return (
         <div className="text-sm mb-4 text-gray flex items-center gap-2">
-            <Link className="text-gray-500" to="/projects">Projects</Link>
+            <Link className="text-white" to="/projects">Projects</Link>
                         <span>{">"}</span>
             <Link to={`/projects/${projectId}`}
-            className="hover:underline text-white font-semibold">
+            className="hover:underline text-white">
                 {task.project.title}
             </Link>
 
